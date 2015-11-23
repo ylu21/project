4c7722b7575748edcc015b2d44894ea4b2916c5e
ylu21 <ylu21@u.rochester.edu>
2015-11-22 21:48:51 -0500

    result added

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,26 +2,12 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http){
     console.log("Hellow world from controller");
     
-    $http.get('/seatlist').success(function(res){
+    $http.get('/').success(function(res){
         console.log("i got the requested data");
-        $scope.seatlist = res;
     });
-//    person1 = {
-//        number: '1',
-//        condition: 'yes'
+    
+//    $scope.addSeat = function(){
+//      console.log($scope.seat);   
 //    };
-//    
-//    person2 = {
-//         number: '2',
-//        condition: 'no'
-//    };
-//    
-//     person3 = {
-//         number: '3',
-//        condition: 'yes'
-//    };
-//    
-//    var seatlist = [person1,person2,person3];
-//    $scope.seatlist = seatlist;
     
 }]);
--- server.js
+++ server.js
@@ -2,21 +2,23 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('seats',['seats']);
+//var url = require('url');
+//var url_parts = url.parse(request.url,true);
+//var query = url_parts.query;
 
-//app.get('/',function(req, res){
-//   res.send("Hello world from server.js"); 
-//});
-
-//look for static files that dont change dynamically
-app.use(express.static(__dirname + "/public"));
-app.get('/seatlist',function(req,res){
-    console.log("I receive a GET request"+res);
-//    find
-    db.seats.find(function(err,docs){
-        console.log(docs);
-        //send back to controller
-        res.json(docs);
-    });
+//display result on homepage
+app.get('/',function(req, res){
+   res.send("Seat yo: "+req.query.result); 
 });
+//look for static files that dont change dyns
+app.use(express.static(__dirname + "/public"));
+//p.get('/seatlist',function(req,res){
+//  console.log("I receive a GET request"+res);
+////  db.seats.find(function(err,docs){
+////      console.log(docs);
+////        //send back to controller
+////        res.json(docs);
+////    });
+//});
 app.listen(3000);
 console.log("server running on port 3000");
