417b7ea0d09e85dbb743dbb5510c373d02fe7516
sabot00 <sabot00@gmail.com>
2015-12-07 14:19:59 -0500

    branch with database implemented

--- server.js
+++ server.js
@@ -16,6 +16,68 @@
 var express = require('express');
 var app = express();
 
+//use with spot
+function findSpot(col, myspot, callback){
+    console.log("why im here");
+    col.find({spot: myspot}).toArray(function(err,result){
+        if(err){}
+        else if(result.length){
+            if(result[0].taken==true){
+                console.log(result[0].spot + "now taken!");
+                col.update({spot: myspot},{$set: {taken: false}});
+                callback(false);
+            }else{
+                 console.log(result[0].spot + "now NOT taken!");
+                col.update({spot: myspot},{$set: {taken: true}});
+                callback(true);
+            }
+        }
+        else{}
+    });
+}
+
+//use with hashspot
+function findHashSpot(col, myspot, callback){
+    console.log("why im here");
+    col.find({hash: myspot}).toArray(function(err,result){
+        if(err){}
+        else if(result.length){
+            if(result[0].taken==true){
+                console.log(result[0].spot + "now taken!");
+                col.update({hash: myspot},{$set: {taken: false}});
+                callback(false);
+            }else{
+                 console.log(result[0].spot + "now NOT taken!");
+                col.update({hash: myspot},{$set: {taken: true}});
+                callback(true);
+            }
+        }
+        else{}
+    });
+}
+
+//use with map
+function checkTaken(col, callback){
+    var myArray = [];
+    var j=0;
+    col.find({taken: true}).toArray(function(err,result){
+        if(err){}
+        else if(result.length){
+            //console.log("found yo: ", result.length);
+            //if found, update taken
+            result.forEach(function(entry){
+                myArray[j]=entry.locationx;
+                myArray[j+1]=entry.locationy;
+                myArray[j+2]=entry.sizex;
+                myArray[j+3]=entry.sizey;
+                j=j+4;
+            });
+            callback(myArray);
+        }
+        else{
+        }
+    });
+}
 
 // required to support parsing of POST request bodies
 var bodyParser = require('body-parser');
@@ -40,13 +102,13 @@ app.use(express.static('frontend'));
 // database can be modified at will.
 var fakeDatabase = [
 //locations are fixed pixels on image (.jpg), scaling happens in canvas draw
-  {area: 'Gleason', hash: '84EBC8AC9D905C20014AD355ECD85110', spot: 'GleasonTable1', taken: false, locationx: 1808, locationy: 216, sizex: 130, sizey: 55},
-  {area: 'Gleason', hash: 'AE21AB7EBE326646E7F9A2EEEFC6646C', spot: 'GleasonTable2', taken: false, locationx: 1808, locationy: 320, sizex: 130, sizey: 55},
-  {area: 'Gleason', hash: '161987612352A37F58F82D373C2DC4DA', spot: 'GleasonTable3', taken: false, locationx: 1808, locationy: 430, sizex: 130, sizey: 55},
-  {area: 'Gleason', hash: 'BFC526BF21C589EA72B93C6B0595C0C0', spot: 'GleasonTable4', taken: false, locationx: 1808, locationy: 526, sizex: 130, sizey: 55},
-  {area: 'Gleason', hash: '96DCD789605E3F39947BDE612633A03D', spot: 'GleasonTable5', taken: false, locationx: 1808, locationy: 620, sizex: 130, sizey: 55},
-  {area: 'Gleason', hash: '3FC2FFE87B9E30D73A1F171FD1AFCD28', spot: 'GleasonTable6', taken: false, locationx: 1808, locationy: 718, sizex: 130, sizey: 55},
-  {area: 'Gleason', hash: '75F7C9E77F7C93744992CEA90D6DE80A', spot: 'GleasonTable7', taken: false, locationx: 1808, locationy: 834, sizex: 130, sizey: 55},
+  {area: 'Gleason', hash: '84EBC8AC9D905C20014AD355ECD85110', spot: 'GleasonTable1', taken: false, locationx: 1808, locationy: 216, sizex: 130, sizey: 54},
+  {area: 'Gleason', hash: 'AE21AB7EBE326646E7F9A2EEEFC6646C', spot: 'GleasonTable2', taken: false, locationx: 1808, locationy: 320, sizex: 130, sizey: 54},
+  {area: 'Gleason', hash: '161987612352A37F58F82D373C2DC4DA', spot: 'GleasonTable3', taken: false, locationx: 1808, locationy: 430, sizex: 130, sizey: 54},
+  {area: 'Gleason', hash: 'BFC526BF21C589EA72B93C6B0595C0C0', spot: 'GleasonTable4', taken: false, locationx: 1808, locationy: 526, sizex: 130, sizey: 54},
+  {area: 'Gleason', hash: '96DCD789605E3F39947BDE612633A03D', spot: 'GleasonTable5', taken: false, locationx: 1808, locationy: 620, sizex: 130, sizey: 54},
+  {area: 'Gleason', hash: '3FC2FFE87B9E30D73A1F171FD1AFCD28', spot: 'GleasonTable6', taken: false, locationx: 1808, locationy: 718, sizex: 130, sizey: 54},
+  {area: 'Gleason', hash: '75F7C9E77F7C93744992CEA90D6DE80A', spot: 'GleasonTable7', taken: false, locationx: 1808, locationy: 834, sizex: 130, sizey: 54},
   {area: 'Carlson', hash: '704B048750C871504927FDE5DAE72D2F', spot: 'CarlsonTable1', taken: false, locationx: 112, locationy: 164, sizex: 20, sizey: 20},
   {area: 'Carlson', hash: '5A8FAF8311F674498A2FDC2246D1B8E4', spot: 'CarlsonTable2', taken: false, locationx: 167, locationy: 142, sizex: 13, sizey: 38},
   {area: 'Carlson', hash: '9E18E3FD872C6F22C3AA1723DCF5C321', spot: 'CarlsonTable3', taken: false, locationx: 201, locationy: 143, sizex: 13, sizey: 38},
@@ -59,6 +121,26 @@ var fakeDatabase = [
   {area: 'Carlson', hash: 'C68D879301CD047BC3029B2032DF3E4F', spot: 'CarlsonTable10', taken: false, locationx: 120, locationy: 470, sizex: 20, sizey: 20},
   {area: 'Carlson', hash: '9F1E0284EE82217A948514268E978FD6', spot: 'CarlsonTable11', taken: false, locationx: 165, locationy: 453, sizex: 20, sizey: 20},
 ];
+
+//database
+var mongodb = require('mongodb');
+var mongojs = require('mongojs');
+var url = 'mongodb://localhost:27017/mydb';
+var db = mongojs(url);
+var roomcollection = db.collection('room');
+var k = 1;
+//db.room.createIndex( { "hash": 1 } );
+roomcollection.ensureIndex({hash:1}, {unique:true});
+for (var i = 0; i < fakeDatabase.length; i++) {
+    roomcollection.insert(fakeDatabase[i],function(err, result){
+            if(err){
+
+            }
+            else{
+                console.log("inserted: "+i, result);
+            }
+        });
+}
 
 
 // REST API as described in http://pgbovine.net/rest-web-api-basics.htm
@@ -113,25 +195,35 @@ app.post('/spots', function (req, res) {
     return; // return early!
   }
 
-  // check if user's name is already in database; if so, send an error
-  for (var i = 0; i < fakeDatabase.length; i++) {
-    var temp = fakeDatabase[i];
-    if (temp.spot == mySpot) {
-        if(temp.taken==true){
-            temp.taken = false;
-            response = 'untake';
-            console.log(temp.spot + "now NOT taken!");
-        }
-        else{
-            temp.taken = true;
-            response = 'take';
-            console.log(temp.spot + "now taken!");
-        }
-
-
-      break; // get out of here early!
-    }
-  }
+  //alternate between take and untake
+  findSpot(roomcollection, mySpot, function(boo){
+      if(boo){
+        response = 'take';
+      }else{
+        response = 'untake';
+      }
+  });
+//  // check if user's name is already in database; if so, send an error
+//  for (var i = 0; i < fakeDatabase.length; i++) {
+//    var temp = fakeDatabase[i];
+//    if (temp.spot == mySpot) {
+//        if(temp.taken==true){
+//            query(roomcollection, temp.hash);
+//            temp.taken = false;
+//            response = 'untake';
+//            console.log(temp.spot + "now NOT taken!");
+//        }
+//        else{
+//            query(roomcollection, temp.hash);
+//            temp.taken = true;
+//            response = 'take';
+//            console.log(temp.spot + "now taken!");
+//        }
+//
+//
+//      break; // get out of here early!
+//    }
+//  }
 
   // otherwise add the user to the database by pushing (appending)
   // postBody to the fakeDatabase list
@@ -152,22 +244,13 @@ var mySpot = req.params[0];
   }
 
   //check if hash is in table
-  for (var i = 0; i < fakeDatabase.length; i++) {
-    var temp = fakeDatabase[i];
-    if (temp.hash == mySpot) {
-        if(temp.taken==true){
-            temp.taken = false;
-            console.log(temp.spot + "now NOT taken!");
-        }
-        else{
-            temp.taken = true;
-            console.log(temp.spot + "now taken!");
-        }
-
-
-      break; // get out of here early!
-    }
-  }
+  findHashSpot(roomcollection, mySpot, function(boo){
+      if(boo){
+        response = 'take';
+      }else{
+        response = 'untake';
+      }
+  });
 
   // otherwise add the user to the database by pushing (appending)
   // postBody to the fakeDatabase list
@@ -185,24 +268,12 @@ var mySpot = req.params[0];
 //   curl -X GET http://localhost:3000/users/Jane
 app.get('/maps/*', function (req, res) {
   var userSpot = req.params[0]; // this matches the '*' part of '/users/*' above
-  // try to look up in fakeDatabase
-  var myArray = [];
-
-  var j = 0;
-  for (var i = 0; i < fakeDatabase.length; i++) {
-
-      if(fakeDatabase[i].area==userSpot && fakeDatabase[i].taken){
-       myArray[j]=fakeDatabase[i].locationx;
-       myArray[j+1]=fakeDatabase[i].locationy;
-       myArray[j+2]=fakeDatabase[i].sizex;
-       myArray[j+3]=fakeDatabase[i].sizey;
-       j=j+4;
-
-       console.log(fakeDatabase[i].locationx);
-   }
-  }
-
-  res.send(myArray);
+  //look up in database
+  checkTaken(roomcollection, function(arr){
+      if(arr.length){
+          res.send(arr);
+      }
+  });
   return;
 
   res.send('{}'); // failed, so return an empty JSON object!
