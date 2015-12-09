e2b6b28b46bed2cc171d318e463601cc6e7efb09
sabot00 <sabot00@gmail.com>
2015-12-09 01:04:02 -0500

    added a lot, LOT, more to the about.html, detailing user eval, team, challenges, future, and more.

--- server.js
+++ server.js
@@ -17,13 +17,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// put all of your static files (e.g., HTML, CSS, JS, JPG) in the static_files/
-// sub-directory, and the server will serve them from there. e.g.,:
-//
-// http://localhost:3000/fakebook.html
-// http://localhost:3000/cat.jpg
-//
-// will send the file static_files/cat.jpg to the user's Web browser
+
 app.use(express.static('frontend'));
 
 
@@ -55,43 +49,9 @@ var fakeDatabase = [
   {area: 'Carlson', hash: '9F1E0284EE82217A948514268E978FD6', spot: 'CarlsonTable11', taken: false, locationx: 165, locationy: 453, sizex: 20, sizey: 20},
 ];
 
+var commentDatabase = [
 
-// REST API as described in http://pgbovine.net/rest-web-api-basics.htm
-
-/* Run through a full API test session with curl (commands typed after '$',
-   outputs shown on the line below each command) ...
-
-$ curl -X GET http://localhost:3000/users
-["Philip","Jane","John"]
-
-$ curl -X GET http://localhost:3000/users/Philip
-{"name":"Philip","job":"professor","pet":"cat.jpg"}
-
-$ curl -X PUT --data "job=bear_wrangler&pet=bear.jpg" http://localhost:3000/users/Philip
-OK
-
-$ curl -X GET http://localhost:3000/users/Philip
-{"name":"Philip","job":"bear_wrangler","pet":"bear.jpg"}
-
-$ curl -X DELETE http://localhost:3000/users/Philip
-OK
-
-$ curl -X GET http://localhost:3000/users/Philip
-{}
-
-$ curl -X GET http://localhost:3000/users
-["Jane","John"]
-
-$ curl -X POST --data "name=Carol&job=scientist&pet=dog.jpg" http://localhost:3000/users
-OK
-
-$ curl -X GET http://localhost:3000/users
-["Jane","John","Carol"]
-
-$ curl -X GET http://localhost:3000/users/Carol
-{"name":"Carol","job":"scientist","pet":"dog.jpg"}
-
-*/
+];
 
 // CREATE a new user
 //
@@ -127,6 +87,30 @@ app.post('/spots', function (req, res) {
       break; // get out of here early!
     }
   }
+
+//comments post request
+app.post('/comments', function (req, res) {
+    var postBody = req.body;
+    
+    // check if user's name is already in database; if so, send an error
+    for (var i = 0; i < fakeDatabase.length; i++) {
+      var temp = fakeDatabase[i];
+      if (temp.spot == mySpot) {
+          if(temp.taken==true){
+              temp.taken = false;
+              response = 'untake';
+              console.log(temp.spot + "now NOT taken!");
+          }
+          else{
+              temp.taken = true;
+              response = 'take';
+              console.log(temp.spot + "now taken!");
+          }
+
+
+        break; // get out of here early!
+      }
+    }
 
   // otherwise add the user to the database by pushing (appending)
   // postBody to the fakeDatabase list
@@ -173,14 +157,8 @@ var mySpot = req.params[0];
 });
 
 
-// READ profile data for a user
-//
-// To test with curl, run:
-//   curl -X GET http://localhost:3000/users/Philip
-//   curl -X GET http://localhost:3000/users/Jane
 app.get('/maps/*', function (req, res) {
-  var userSpot = req.params[0]; // this matches the '*' part of '/users/*' above
-  // try to look up in fakeDatabase
+  var userSpot = req.params[0];
   var myArray = [];
 
   var j = 0;
@@ -201,64 +179,4 @@ app.get('/maps/*', function (req, res) {
   return;
 
   res.send('{}'); // failed, so return an empty JSON object!
-});
-
-
-// READ a list of all usernames (note that there's no '*' at the end)
-//
-// To test with curl, run:
-//   curl -X GET http://localhost:3000/users
-
-
-// UPDATE a user's profile with the data given in POST
-//
-// To test with curl, run:
-//   curl -X PUT --data "job=bear_wrangler&pet=bear.jpg" http://localhost:3000/users/Philip
-app.put('/users/*', function (req, res) {
-  var nameToLookup = req.params[0]; // this matches the '*' part of '/users/*' above
-  // try to look up in fakeDatabase
-  for (var i = 0; i < fakeDatabase.length; i++) {
-    var e = fakeDatabase[i];
-    if (e.name == nameToLookup) {
-      // update all key/value pairs in e with data from the post body
-      var postBody = req.body;
-      for (key in postBody) {
-        var value = postBody[key];
-        e[key] = value;
-      }
-
-      res.send('OK');
-      return; // return early!
-    }
-  }
-
-  res.send('ERROR'); // nobody in the database matches nameToLookup
-});
-
-
-// DELETE a user
-//
-// To test with curl, run:
-//   curl -X DELETE http://localhost:3000/users/Philip
-//   curl -X DELETE http://localhost:3000/users/Jane
-app.delete('/users/*', function (req, res) {
-  var nameToLookup = req.params[0]; // this matches the '*' part of '/users/*' above
-  // try to look up in fakeDatabase
-  for (var i = 0; i < fakeDatabase.length; i++) {
-    var e = fakeDatabase[i];
-    if (e.name == nameToLookup) {
-      fakeDatabase.splice(i, 1); // remove current element at index i
-      res.send('OK');
-      return; // return early!
-    }
-  }
-
-  res.send('ERROR'); // nobody in the database matches nameToLookup
-});
-
-
-// start the server on http://localhost:3000/
-var server = app.listen(3000, function () {
-  var port = server.address().port;
-  console.log('Server started at http://localhost:%s/', port);
 });
